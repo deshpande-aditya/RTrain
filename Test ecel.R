a60a35f5b4723267f56b2d5225975cc377b725d2
Aditya Deshpande <72786936+deshpande-aditya@users.noreply.github.com>
2020-10-18 18:36:47 +0530

    add new files

---
+++ Test ecel.R
@@ -0,0 +1,6 @@
+library('dplyr')
+install.packages("tidyverse")
+library("tidyverse")
+# Read in the data
+baseline <- read.csv("~/R/RTrain/R-Tutorial-Data-Files-master/R-Tutorial-Data-Files-master/Baseline.csv")
+print(df)
